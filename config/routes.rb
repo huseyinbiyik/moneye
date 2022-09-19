b5069ce1d8ea859fcfd044f13af724608aa71e29
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-09-19 15:16:01 +0300

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :expenses_groups
-  resources :groups
+  resources :groups do
   resources :expenses
+  end
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "groups#index"
 end
