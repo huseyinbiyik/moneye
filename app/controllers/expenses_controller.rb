3e94220c4684c1d9c488db79b2aa782fbbaf1d12
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-09-20 21:02:54 +0300

    Fix expense group pick feature

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -22,14 +22,11 @@ class ExpensesController < ApplicationController
 
   # POST /expenses or /expenses.json
   def create
-    @expense = Expense.new(expense_params)
-    @expense.user_id = current_user.id
-    @group = params[:group_id]
-
+    @group = Group.find(params[:expense][:group_id])
+    @expense = @group.expenses.create(name: params[:expense][:name], amount: params[:expense][:amount], user_id: current_user.id)
     respond_to do |format|
       if @expense.save
-        ExpensesGroup.create(expense_id: @expense.id, group_id: @group)
-        format.html { redirect_to group_expenses_path, notice: 'Expense was successfully created.' }
+        format.html { redirect_to groups_path, notice: 'Expense was successfully created.' }
         format.json { render :show, status: :created, location: @expense }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -69,7 +66,7 @@ class ExpensesController < ApplicationController
   end
 
   # Only allow a list of trusted parameters through.
-  def expense_params
-    params.require(:expense).permit(:name, :amount)
-  end
+  # def expense_params
+  #   params.permit(:name, :amount, :group_id)
+  # end
 end
