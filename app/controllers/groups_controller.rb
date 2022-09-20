16c3a2300c3c9f86a3b3664368b698e4a16941a2
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-09-20 13:47:46 +0300

    Fix group index page current user issue

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,10 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!, except: :index
   before_action :set_group, only: %i[show edit update destroy]
 
   # GET /groups or /groups.json
   def index
-    @groups = Group.all
+    @groups = current_user.groups if user_signed_in?
   end
 
   # GET /groups/1 or /groups/1.json
